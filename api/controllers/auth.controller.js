bfdf1043f00dab0cb00d5dcd1466a5599ee584cc
Aniket8947 <aniket.21b0101170@abes.ac.in>
2024-07-24 00:37:35 +0530

    added the signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../model/user.model.js";
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) =>{
     const {username, email, password} = req.body;
@@ -15,5 +16,32 @@ export const signup = async (req, res, next) =>{
     } catch (error) {
         next(error)
     }
-    
+}
+
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body;
+    if(!email || !password || email==='' || password===''){
+        next(errorHandler(400, "All fields are required"));
+    }
+
+    try {
+        const validuser = await User.findOne({email});
+        if(!validuser){
+            return next(errorHandler(404, 'User not found'))
+        }
+        const validpass = bcryptjs.compareSync(password, validuser.password);
+        if(!validpass){
+            return next(errorHandler(400, 'Invalid Password'));
+        }
+        const token = jwt.sign({id : validuser._id}, process.env.JWT_SECRET_KEY);
+
+        const {password:pass, ...rest} = validuser._doc;
+
+
+        res.status(200).cookie('acess_token', token, {
+            httpOnly: true
+        }).json(rest);
+    } catch (error) {
+        next(error)
+    }
 }
